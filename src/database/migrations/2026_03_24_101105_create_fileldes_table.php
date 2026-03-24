6013a2481d7072fc2e736305cce29a740b5a6f6a
anourhamuze33 <anourhamuze@gmail.com>
2026-03-24 11:22:08 +0100

    feat: migrations/model/service/controller

--- src/database/migrations/2026_03_24_101105_create_fileldes_table.php
+++ src/database/migrations/2026_03_24_101105_create_fileldes_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fileldes', function (Blueprint $table) {
+        Schema::create('fieldes', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('ville_id')->constrained('villes')->onDelete('cascade');
+            $table->float('size');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fileldes');
+        Schema::dropIfExists('fieldes');
     }
 };
