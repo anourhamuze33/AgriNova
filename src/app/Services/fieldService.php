10684fb96bf188b29616801b9bf8f954e439b7c7
anourhamuze33 <anourhamuze@gmail.com>
2026-04-01 11:02:44 +0200

    feat: fields controller services

--- src/app/Services/fieldService.php
+++ src/app/Services/fieldService.php
@@ -2,10 +2,39 @@
 
 namespace App\Services;
 
-class fieldService
+use App\Repositories\Fieldes\FieldRepository;
+
+class FieldService
 {
-    public function __construct()
+    protected FieldRepository $fieldRepository;
+
+    public function __construct(FieldRepository $fieldRepository)
     {
-        //
+        $this->fieldRepository = $fieldRepository;
+    }
+
+    public function getAllFields()
+    {
+        return $this->fieldRepository->getAll();
+    }
+
+    public function getFieldById($id)
+    {
+        return $this->fieldRepository->findById($id);
+    }
+
+    public function createField(array $data)
+    {
+        return $this->fieldRepository->create($data);
+    }
+
+    public function updateField($id, array $data)
+    {
+        return $this->fieldRepository->update($id, $data);
+    }
+
+    public function deleteField($id)
+    {
+        return $this->fieldRepository->delete($id);
     }
 }
